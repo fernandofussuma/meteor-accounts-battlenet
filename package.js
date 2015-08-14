e06724e2c206bf4c063e432291db097331a0b74b
Fernando Fussuma <fernandofussuma@gmail.com>
2015-08-14 14:45:31 -0300

    battlenet@0.0.2

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'fernandofussuma:accounts-battlenet',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
+  version: '0.0.2',
   summary: 'Login service for Battlenet accounts',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/fernandofussuma/meteor-accounts-battlenet.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
@@ -16,7 +12,7 @@ Package.onUse(function(api) {
   api.use('accounts-base', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('fernandofussuma:battlenet@0.0.1', ['client', 'server']);
+  api.use('fernandofussuma:battlenet@0.0.2', ['client', 'server']);
 
   api.addFiles('battlenet_login_button.css', 'client');
 
